bd58e17732ea739fbde311ec25712698c54c86e9
dvvhostua <dvvhostua@gmail.com>
2017-02-27 10:35:07 +0100

    DAY_UNIT TickHandler added to update calendar

--- src/c/main.c
+++ src/c/main.c
@@ -65,9 +65,14 @@ int month_length(struct tm * ptm){
 }
 
 
-static void update_calendar(struct tm *tick_time){
+static void update_calendar(){
   //get week day from tm-struct(0-Sunday, 6-Saturday)
   //convert to 0-Monday, 6-Sunday format
+  
+  // Get a tm structure
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+  
   int i; 
   int weekday = tick_time->tm_wday == 0 ? 6 : (tick_time->tm_wday - 1);
   int firstday = tick_time->tm_mday - weekday;//Begin of the week
@@ -174,12 +179,15 @@ static void update_time() {
   // Display this date on the TextLayer
   text_layer_set_text(s_date_layer, s_date_buffer);
   
-  update_calendar(tick_time);
 }
 
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
+}
+
+static void day_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_calendar();
 }
 
 static void main_window_load(Window *window) {
@@ -189,6 +197,7 @@ static void main_window_load(Window *window) {
   
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  tick_timer_service_subscribe(DAY_UNIT, day_handler);
 
   // Get information about the Window
   Layer *window_layer = window_get_root_layer(window);
@@ -269,6 +278,7 @@ static void init() {
   
   // Make sure the time is displayed from the start
   update_time();
+  update_calendar();
 
 }
 
